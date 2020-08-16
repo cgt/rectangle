5ba7dc37e6f0c38bdbb98afdcd761fe479e03ada
Christoffer G. Thomsen <chris@cgt.name>
2020-08-16 18:14:16 +0200

    It moves and stuff

--- main.lua
+++ main.lua
@@ -16,14 +16,22 @@ function love.load()
 		vsync = true
 	})
 
+	score_font = love.graphics.newFont(40)
+
 	frames = 0
 	tooClose = false
-	ox = 30
-	oy = 30
-	ow = 15
-	oh = 15
+	o = {}
+	o.x = 30
+	o.y = 30
+	o.w = 15
+	o.h = 15
 	next_update = 0
-	won = false
+	green_box_frame = -1
+	score = 0
+	direction = {}
+	direction.x = 1
+	direction.y = 1
+	direction.last_changed = 0
 end
 
 function love.keypressed(key)
@@ -34,28 +42,45 @@ end
 
 function love.mousepressed(x, y)
 	x, y = push:toGame(x, y)
-	distx = math.abs(x-ox)
-	disty = math.abs(y-oy)
-	if distx < ow and disty < oh then
-		won = true
+	distx = math.abs(x-o.x)
+	disty = math.abs(y-o.y)
+	if distx < o.w and disty < o.h then
+		green_box_frame = frames
+		score = score + 1
 	end
 end
 
 function love.update(dt)
 	frames = frames + 1
 
-	if not won then
-		x, y = push:toGame(love.mouse.getPosition())
-		distx = math.abs(x-ox)
-		disty = math.abs(y-oy)
-		if not tooClose and distx < ow*2 and disty < oh*2 then
-			next_update = frames + math.random(20)
-			tooClose = true
-		elseif tooClose and frames == next_update then
-			tooClose = false
-			next_update = false
-			ox = math.random(VW-20)
-			oy = math.random(VH-20)
+	x, y = push:toGame(love.mouse.getPosition())
+	distx = math.abs(x-o.x)
+	disty = math.abs(y-o.y)
+	if not tooClose and distx < o.w*2 and disty < o.h*2 then
+		next_update = frames + math.random(20)
+		tooClose = true
+	elseif tooClose and frames == next_update then
+		tooClose = false
+		next_update = false
+		o.x = math.random(VW-20)
+		o.y = math.random(VH-20)
+	else
+		o.x = o.x + direction.x
+		o.y = o.y + direction.y
+		if o.x > VH-25 then
+			o.x = 25
+		end
+		if o.y > VH-25 then
+			o.y = 25
+		end
+		if direction.last_changed < frames-60 then
+			which = math.random(0, 1)
+			if which == 1 then
+				direction.x = math.random(-2, 2)
+			else
+				direction.y = math.random(-2, 2)
+			end
+			direction.last_changed = frames
 		end
 	end
 end
@@ -63,14 +88,18 @@ end
 function love.draw()
 	push:start()
 
-	if won then
-		love.graphics.setColor(0, 255, 0)
+	if green_box_frame ~= -1 and green_box_frame > frames-120 then
+		love.graphics.setColor(0, 128, 0)
 	elseif tooClose then
 		love.graphics.setColor(255, 191, 0)
 	else
 		love.graphics.setColor(255, 255, 255)
 	end
-	love.graphics.rectangle('fill', ox, oy, ow, oh)
+	love.graphics.rectangle('fill', o.x, o.y, o.w, o.h)
+
+	love.graphics.setColor(255, 255, 255)
+	love.graphics.setFont(score_font)
+	love.graphics.print(score)
 
 	push:finish()
 end
